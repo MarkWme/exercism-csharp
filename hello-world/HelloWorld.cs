f5b170e573188d4c03653ee36a23bda7390262d3
Mark Whitby <m@markw.me>
2019-02-15 22:09:38 +0000

    Initial commit

---
+++ hello-world/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class HelloWorld
+{
+    public static string Hello()
+    {
+        const string greeting = "Hello, World!";
+        return greeting;
+    }
+}
---
+++ pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        input = input.ToUpper();
+        int count = 0;
+        bool returnValue = false;
+        for(int check = 65; check <= 90; check++) {
+            if(input.Contains((char)check)) {
+                count++;
+            }
+        }
+        if(count>=26) returnValue = true;
+        return(returnValue);
+    }
+}
